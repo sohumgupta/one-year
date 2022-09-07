2d6bc32e0a61266ebe4063b75f615e19b65b740a
sohumgupta <sohumgupta99@gmail.com>
2022-09-07 21:21:01 +0000

    add more letters :)

--- src/Letters.js
+++ src/Letters.js
@@ -1,9 +1,15 @@
 import FigJam from './assets/FigJam.png';
 import Chapstick from './assets/Chapstick.png';
+import AsteriosPolyp from './assets/AsteriosPolyp.png';
+import GingkoLeafCard from './assets/GingkoLeafCard.png';
+import MonroeDairyBox from './assets/MonroeDairyBox.png';
 
 const letters = [
     { date: "09-07-2021", image: FigJam, text: "This was the first gift you ever gave me. I know that you don't like fig jam that much, but it was genuinely one of the best tasting jams I've ever had. I licked it off your finger, I think. It was a huge turn-on."},
-    { date: "09-09-2022", image: Chapstick, text: "This was the first gift I ever gave you. I didn't even really look for chapstick when you asked me, I just went straight to Eastside Mini Mart. I thought I was so smart when I took it out of the packaging too :)"}
+    { date: "09-09-2021", image: Chapstick, text: "This was the first gift I ever gave you. I didn't even really look for chapstick when you asked me, I just went straight to Eastside Mini Mart. I thought I was so smart when I took it out of the packaging too :)"},
+    { date: "12-18-2021", image: AsteriosPolyp, text: "Asterios Polyp, my Bible, and David Mazzuchelli, my Father, Son, and Holy Spirit. Thank you for communicating with the gods on my behalf. And thank you for caring so much about the silly stuff I like."},
+    { date: "12-18-2021", image: GingkoLeafCard, text: "I remember it had rained, and I had to try to find Gingko leaves that were at least somewhat dry. The internet said they'd take several weeks to dry, but I think I only left them out for a couple of days."},
+    { date: "10-02-2021", image: MonroeDairyBox, text: "I still can't believe you got this for me. We had known each other for less than a month, and I just could have never guessed. I don't think I will ever forget how I felt then. And I won't forget how you looked either ;)"}
 ]
 
 export default letters;
